3cc9c9a38e33aad58f5635e078a30853d6261303
Irvin Senwedi <115619937+Khulow@users.noreply.github.com>
2025-11-14 10:05:41 +0200

    style: bold white bottom nav labels

--- lib/views/main_navigation_screen.dart
+++ lib/views/main_navigation_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:past_question_paper_v1/providers/navigation_providers.dart';
+import 'package:past_question_paper_v1/utils/app_colors.dart';
 import 'package:past_question_paper_v1/views/home_screen.dart';
 import 'package:past_question_paper_v1/viewmodels/session_history_viewmodel.dart';
 import 'package:past_question_paper_v1/views/history/pqp_history_screen.dart';
@@ -25,6 +26,10 @@ class MainNavigationScreen extends ConsumerWidget {
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         currentIndex: currentIndex,
+        selectedItemColor: AppColors.neutralCard,
+        unselectedItemColor: AppColors.neutralCard.withOpacity(0.7),
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w700),
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.w700),
         onTap: (index) {
           ref.read(bottomNavigationProvider.notifier).setIndex(index);
           if (index == 1) {
